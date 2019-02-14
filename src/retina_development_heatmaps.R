4c1c68b7192489b687453a478088ed06f0c05059
David McGaughey <mcgaughey@gmail.com>
2019-02-14 15:20:03 -0500

    change colors

--- src/retina_development_heatmaps.R
+++ src/retina_development_heatmaps.R
@@ -4,8 +4,9 @@ library(tidyverse)
 library(circlize)
 rgc <- c('GAP43', 'POU4F1', 'ISL1', 'POU4F2','ATOH7','DLX2','SHH','SLX2')
 pr <- c('OTX2','RCVRN','AIPL1','NRL','CRX','PDE6B','NR2E3','ROM1','GNGT2','GNAT1','PDE6H','CNGB1','OPN1SW','GUCA1A','GNAT2','CNGA1','RHO','OPN1MW')
+progenitor <- c('VSX2','SOX2','SOX9','ASCL1','SFRP2','HES1','LHX2','PRTG','LGR5','ZIC1','DLL3','GLI1','FGF19','LIN28B')
 
-gene <- pr
+gene <- progenitor
 query = paste0('select * from lsTPM_gene where ID in ("',paste(gene, collapse='","'),'")')
 p <- dbGetQuery(gene_pool_2019, query) %>% left_join(.,core_tight_2019) %>% 
   left_join(., gene_pool_2019 %>% tbl('gene_IDs') %>% as.tibble()) %>% 
@@ -45,7 +46,8 @@ colnames(y) <- y['Days',]
 y <- y[-1,]
 
 two <- Heatmap(log2(y), cluster_columns = F, column_title = 'Swaroop 3D Organoid\nGFP Neg',
-               col = colorRamp2(c(-0, 15), viridis(2)))
+               col = colorRamp2(c(-0, 15), viridis(2)),
+               show_heatmap_legend = F)
 
 x <- rbind(organoid_swaroop_GFPneg, ESC)
 y <- x %>% spread(ID, value) %>% t()
@@ -53,7 +55,8 @@ colnames(y) <- y['Days',]
 y <- y[-1,]
 
 three <- Heatmap(log2(y), cluster_columns = F, column_title = 'Swaroop 3D Organoid\nGFP Neg',
-               col = colorRamp2(c(-0, 15), viridis(2)))
+               col = colorRamp2(c(-0, 15), viridis(2)),
+               show_heatmap_legend = F)
 
 x <- rbind(organoid_johnston, ESC)
 y <- x %>% spread(ID, value) %>% t()
@@ -61,6 +64,7 @@ colnames(y) <- y['Days',]
 y <- y[-1,]
 
 four <- Heatmap(log2(y), cluster_columns = F, column_title = 'Johnston 3D Organoid', 
-                 col = colorRamp2(c(0, 15), viridis(2)))
+                 col = colorRamp2(c(0, 15), viridis(2)),
+                show_heatmap_legend = F)
 
 one + two + three + four
