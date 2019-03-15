1626865733d8580345cc7243597a31f0a3ce4fc9
David McGaughey <mcgaughey@gmail.com>
2019-03-15 12:55:24 -0400

    added both sup materials

--- src/limmaTest_sup.R
+++ src/limmaTest_sup.R
@@ -1,2 +1,3 @@
 load('data/data_pull.Rdata')
 WriteXLS::WriteXLS(q01_limma_gene_sig, ExcelFileName  ='supplementary_materials/supplementary_files__limmaTests.xls')
+WriteXLS::WriteXLS(core_tight_2019, ExcelFileName  ='supplementary_materials/supplementary_files__metadata.xls')
